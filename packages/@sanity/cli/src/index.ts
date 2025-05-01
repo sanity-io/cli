6f9143ab33d2612277ab5152a3e3a0342109521f
Espen Hovlandsdal <espen@hovlandsdal.com>
2025-05-01 08:36:45 -0700

    refactor: add stubs for old exports that needs to be there

--- packages/@sanity/cli/src/index.ts
+++ packages/@sanity/cli/src/index.ts
@@ -1,1 +1,28 @@
-export {run} from '@oclif/core'
+/**
+ * These are needed for backwards compatibility with the old CLI!
+ * To be implemented!
+ */
+
+// @todo implement
+export function getCliClient(_options: any): any {
+  throw new Error('@todo not implemented yet')
+}
+
+// @todo implement typings
+export function defineCliConfig(config: any) {
+  return config
+}
+
+// @todo implement typings
+export function createCliConfig(config: any) {
+  return config
+}
+
+// @todo implement
+export function loadEnv(
+  mode: string,
+  envDir: string,
+  _prefixes: string[] = ['VITE_'],
+): Record<string, string> {
+  return {}
+}
