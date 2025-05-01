4c45f68e72c7f93ef83544d14ca83fead2f0397a
Espen Hovlandsdal <espen@hovlandsdal.com>
2025-05-01 08:49:25 -0700

    refactor: add manage command

---
+++ packages/@sanity/cli/src/commands/manage.ts
@@ -0,0 +1,46 @@
+import type {FlagInput} from '@oclif/core/interfaces'
+
+import {Command} from '@oclif/core'
+import open from 'open'
+
+import {getCliConfig} from '../config/cli/getCliConfig.js'
+import {findProjectRoot} from '../config/findProjectRoot.js'
+import {getStudioConfig} from '../config/studio/getStudioConfig.js'
+
+export default class ManageCommand extends Command {
+  static override description = 'Opens project management interface in your web browser'
+  static override flags = {} satisfies FlagInput
+
+  public async run(): Promise<void> {
+    // Parse to ensure no invalid flags are passed
+    await this.parse(ManageCommand)
+
+    const projectId = await findProjectId()
+
+    const url = projectId
+      ? `https://www.sanity.io/manage/project/${projectId}`
+      : 'https://www.sanity.io/manage/'
+
+    this.log(`Opening ${url}`)
+    await open(url)
+  }
+}
+
+async function findProjectId() {
+  const root = await findProjectRoot(process.cwd())
+  if (!root) {
+    return null
+  }
+
+  const cliConfig = await getCliConfig(root.directory)
+  if (cliConfig.api?.projectId) {
+    return cliConfig.api.projectId
+  }
+
+  const config = await getStudioConfig(root.directory, {resolvePlugins: false})
+  if (!Array.isArray(config)) {
+    return config.projectId || null
+  }
+
+  return null
+}
