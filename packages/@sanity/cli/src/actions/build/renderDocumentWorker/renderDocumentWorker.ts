8b3d1281eefab19e6e0db5f8ba09a98a164f976a
Binoy Patel <me@binoy.io>
2025-06-18 23:31:39 -0400

    test: add integration level tests

---
+++ packages/@sanity/cli/src/actions/build/renderDocumentWorker/renderDocumentWorker.ts
@@ -0,0 +1,54 @@
+import {type MessagePort} from 'node:worker_threads'
+
+import {getDocumentHtml} from './getDocumentHtml.jsx'
+import {type DocumentProps} from './types.js'
+
+/**
+ * @internal
+ */
+export interface RenderDocumentWorkerOptions {
+  studioRootPath: string
+
+  importMap?: {
+    imports?: Record<string, string>
+  }
+  isApp?: boolean
+  props?: DocumentProps
+}
+
+/**
+ * Renders a document in a worker thread
+ *
+ * @param parent - The parent port to send messages to
+ * @param options - The options for the document to render
+ * @returns - The rendered document
+ */
+export async function renderDocumentWorker(
+  parent: MessagePort,
+  options: RenderDocumentWorkerOptions,
+) {
+  const {importMap, isApp, props, studioRootPath} = options
+
+  if (typeof studioRootPath !== 'string') {
+    parent.postMessage({
+      message: 'Missing/invalid `studioRootPath` option',
+      type: 'error',
+    })
+    return
+  }
+
+  if (props && typeof props !== 'object') {
+    parent.postMessage({
+      message: '`props` must be an object if provided',
+      type: 'error',
+    })
+    return
+  }
+
+  const html = await getDocumentHtml(parent, studioRootPath, props, importMap, isApp)
+
+  parent.postMessage({
+    html,
+    type: 'result',
+  })
+}
