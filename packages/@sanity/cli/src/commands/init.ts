8b3a50a5ef33264e75f3f3000314a22f4354d088
Espen Hovlandsdal <espen@hovlandsdal.com>
2025-07-02 13:31:50 -0700

    feat(cli): implement minimal init command boilerplate

---
+++ packages/@sanity/cli/src/commands/init.ts
@@ -0,0 +1,158 @@
+import {Args, Command, Flags} from '@oclif/core'
+import {type FlagInput} from '@oclif/core/interfaces'
+
+import {SanityCliCommand} from '../BaseCommand.js'
+
+export class InitCommand extends SanityCliCommand<typeof InitCommand> {
+  static override args = {type: Args.string({hidden: true})}
+  static override description = 'Initialize a new Sanity Studio, project and/or app'
+
+  static override examples = [
+    '<%= config.bin %> <%= command.id %>',
+    {
+      command: '<%= config.bin %> <%= command.id %> --dataset-default',
+      description: 'Initialize a new project with a public dataset named "production"',
+    },
+    {
+      command:
+        '<%= config.bin %> <%= command.id %> -y --project abc123 --dataset production --output-path ~/myproj',
+      description: 'Initialize a project with the given project ID and dataset to the given path',
+    },
+    {
+      command:
+        '<%= config.bin %> <%= command.id %> -y --project abc123 --dataset staging --template moviedb --output-path .',
+      description:
+        'Initialize a project with the given project ID and dataset using the moviedb template to the given path',
+    },
+    {
+      command:
+        '<%= config.bin %> <%= command.id %> -y --create-project "Movies Unlimited" --dataset moviedb --visibility private --template moviedb --output-path /Users/espenh/movies-unlimited',
+      description: 'Create a brand new project with name "Movies Unlimited"',
+    },
+  ] satisfies Array<Command.Example>
+
+  static override flags = {
+    'auto-updates': Flags.boolean({
+      allowNo: true,
+      default: true,
+      description: 'Enable auto updates of studio versions',
+      exclusive: ['bare'],
+    }),
+    bare: Flags.boolean({
+      description:
+        'Skip the Studio initialization and only print the selected project ID and dataset name to stdout',
+    }),
+    coupon: Flags.string({
+      description:
+        'Optionally select a coupon for a new project (cannot be used with --project-plan)',
+      exclusive: ['project-plan'],
+      helpValue: '<code>',
+    }),
+    'create-project': Flags.string({
+      description: 'Create a new project with the given name',
+      helpValue: '<name>',
+    }),
+    dataset: Flags.string({
+      description: 'Dataset name for the studio',
+      exclusive: ['dataset-default'],
+      helpValue: '<name>',
+    }),
+    'dataset-default': Flags.boolean({
+      description: 'Set up a project with a public dataset named "production"',
+    }),
+    env: Flags.string({
+      default: '.env',
+      description: 'Write environment variables to file',
+      exclusive: ['bare'],
+      helpValue: '<filename>',
+    }),
+    'from-create': Flags.boolean({
+      description: 'Internal flag to indicate that the command is run from create-sanity',
+      hidden: true,
+    }),
+    git: Flags.string({
+      default: undefined,
+      description: 'Specify a commit message for initial commit, or disable git init',
+      exclusive: ['bare'],
+      // oclif doesn't indent correctly with custom help labels, thus leading space :/
+      helpLabel: '    --[no-]git',
+      helpValue: '<message>',
+    }),
+    // oclif doesn't support a boolean/string flag combination, but listing both a
+    // `--git` and a `--no-git` flag in help breaks conventions, so we hide this one,
+    // but use it to "combine" the two in the actual logic.
+    'no-git': Flags.boolean({
+      description: 'Disable git initialization',
+      exclusive: ['git'],
+      hidden: true,
+    }),
+    organization: Flags.string({
+      description: 'Organization ID to use for the project',
+      helpValue: '<id>',
+    }),
+    'output-path': Flags.string({
+      description: 'Path to write studio project to',
+      exclusive: ['bare'],
+      helpValue: '<path>',
+    }),
+    'package-manager': Flags.string({
+      description: 'Specify which package manager to use [allowed: npm, yarn, pnpm]',
+      exclusive: ['bare'],
+      helpValue: '<manager>',
+      options: ['npm', 'yarn', 'pnpm'],
+    }),
+    project: Flags.string({
+      description: 'Project ID to use for the studio',
+      helpValue: '<id>',
+    }),
+    'project-plan': Flags.string({
+      description: 'Optionally select a plan for a new project',
+      helpValue: '<name>',
+    }),
+    provider: Flags.string({
+      description: 'Login provider to use',
+      helpValue: '<provider>',
+    }),
+    template: Flags.string({
+      default: 'clean',
+      description: 'Project template to use [default: "clean"]',
+      exclusive: ['bare'],
+      helpValue: '<template>',
+    }),
+    typescript: Flags.boolean({
+      allowNo: true,
+      default: true,
+      description: 'Enable TypeScript support',
+      exclusive: ['bare'],
+    }),
+    visibility: Flags.string({
+      description: 'Visibility mode for dataset',
+      helpValue: '<mode>',
+      options: ['public', 'private'],
+    }),
+    yes: Flags.boolean({
+      char: 'y',
+      default: false,
+      description:
+        'Unattended mode, answers "yes" to any "yes/no" prompt and otherwise uses defaults',
+    }),
+  } satisfies FlagInput
+
+  public async run(): Promise<void> {
+    // For backwards "compatibility" - we used to allow `sanity init plugin`,
+    // and no longer do - but instead of printing an error about an unknown
+    // _command_, we want to acknowledge that the user is trying to do something
+    // that no longer exists but might have at some point in the past.
+    if (this.args.type) {
+      this.error(
+        this.args.type === 'plugin'
+          ? 'Initializing plugins through the CLI is no longer supported'
+          : `Unknown init type "${this.args.type}"`,
+        {exit: 1},
+      )
+      return
+    }
+
+    throw new Error('Not yet implemented')
+  }
+}
