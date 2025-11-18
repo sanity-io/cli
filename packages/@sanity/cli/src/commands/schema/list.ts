d662f14f74b4d0e311ab32abf380aef444eed971
laurenashpole <lauren.ashpole@sanity.io>
2025-11-18 17:23:34 -0500

    feat: migrate schema list command to oclif

--- packages/@sanity/cli/src/commands/schema/list.ts
+++ packages/@sanity/cli/src/commands/schema/list.ts
@@ -1,49 +1,63 @@
-import {type CliCommandDefinition} from '@sanity/cli'
+import {Flags} from '@oclif/core'
+import {SanityCommand} from '@sanity/cli-core'
 
-const description = 'Lists all schemas in the current dataset.'
+const description = `
+Lists all schemas in the current dataset.
 
-const helpText = `
 **Note**: This command is experimental and subject to change.
 
 This operation (re-)generates a manifest file describing the sanity config workspace by default.
 To re-use an existing manifest file, use --no-extract-manifest.
+`.trim()
 
-Options
-  --json get schema as json
-  --id <schema_id> fetch a single schema by id
-  --manifest-dir <directory> directory containing manifest file (default: ./dist/static)
-  --no-extract-manifest disables manifest generation – the command will fail if no manifest exists
+export class ListSchemaCommand extends SanityCommand<typeof ListSchemaCommand> {
+  static override description = description
 
-Examples
-  # List all schemas found in any workspace dataset in a table
-  sanity schema list
+  static override examples = [
+    {
+      command: '<%= config.bin %> <%= command.id %>',
+      description: 'List all schemas found in any workspace dataset in a table',
+    },
+    {
+      command: '<%= config.bin %> <%= command.id %> --id _.schemas.workspaceName',
+      description: 'Get a schema for a given id',
+    },
+    {
+      command: '<%= config.bin %> <%= command.id %> --json',
+      description: 'Get stored schemas as pretty-printed json-array',
+    },
+    {
+      command: '<%= config.bin %> <%= command.id %> --json --id _.schemas.workspaceName',
+      description: 'Get singular stored schema as pretty-printed json-object',
+    },
+    {
+      command: '<%= config.bin %> <%= command.id %> --no-extract-manifest',
+      description:
+        'Runs using a pre-existing manifest file. Config changes in sanity.config will not be picked up in this case.',
+    },
+  ]
 
-  # Get a schema for a given id
-  sanity schema list --id _.schemas.workspaceName
+  static override flags = {
+    id: Flags.string({
+      description: 'Fetch a single schema by id',
+      helpValue: '<schema_id>',
+    }),
+    json: Flags.boolean({
+      description: 'Get schema as json',
+    }),
+    'manifest-dir': Flags.string({
+      default: './dist/static',
+      description: 'Directory containing manifest file',
+      helpValue: '<directory>',
+    }),
+    'no-extract-manifest': Flags.boolean({
+      description: 'Disables manifest generation - the command will fail if no manifest exists',
+    }),
+  }
 
-  # Get stored schemas as pretty-printed json-array
-  sanity schema list --json
+  public async run(): Promise<void> {
+    const {flags} = await this.parse(ListSchemaCommand)
 
-  # Get singular stored schema as pretty-printed json-object
-  sanity schema list --json --id _.schemas.workspaceName
-
-  # Runs using a pre-existing manifest file
-  # Config changes in sanity.config will not be picked up in this case
-  sanity schema list --no-extract-manifest
-`
-
-const fetchSchemaCommand = {
-  name: 'list',
-  group: 'schema',
-  signature: '',
-  description,
-  helpText,
-  action: async (args, context) => {
-    const mod = await import('../../actions/schema/listSchemasAction')
-    const result = await mod.default(args.extOptions, context)
-    if (result === 'failure') process.exit(1)
-    return result
-  },
-} satisfies CliCommandDefinition
-
-export default fetchSchemaCommand
+    this.log(JSON.stringify(flags))
+  }
+}
