a739cf148d4de687c11e5e8a0d25fba657629269
Matthew Dent <mttdnt@gmail.com>
2025-11-19 09:04:58 -0500

    feat(media): move import media command (#171)
    * feat(media): migrating media import prompts to oclif
    
    * feat(media): import media importting functionality and command tests
    
    * feat(media): migrating tests for import functionality
    
    ---------
    
    Co-authored-by: Matthew Dent <matthew.dent@MacBook-Pro.local>

---
+++ packages/@sanity/cli/src/actions/media/importMedia.ts
@@ -0,0 +1,399 @@
+import {createHash} from 'node:crypto'
+import {createReadStream, type ReadStream} from 'node:fs'
+import fs, {mkdtemp} from 'node:fs/promises'
+import {tmpdir} from 'node:os'
+import path from 'node:path'
+import {text} from 'node:stream/consumers'
+import {pipeline} from 'node:stream/promises'
+
+import {spinner} from '@sanity/cli-core'
+import {type SanityClient} from '@sanity/client'
+import {type FileAsset, type ImageAsset, type SanityDocument} from '@sanity/types'
+import chalk from 'chalk'
+import gunzipMaybe from 'gunzip-maybe'
+// @ts-expect-error `peek-stream` module currently untyped
+import isTar from 'is-tar'
+// @ts-expect-error `peek-stream` module currently untyped
+import peek from 'peek-stream'
+import {
+  catchError,
+  EMPTY,
+  filter,
+  from,
+  map,
+  mergeMap,
+  mergeWith,
+  type Observable,
+  of,
+  type OperatorFunction,
+  switchMap,
+  tap,
+  zip,
+} from 'rxjs'
+import tar from 'tar-fs'
+import {glob} from 'tinyglobby'
+
+import {findNdjsonEntry} from '../../util/findNdjsonEntry.js'
+import {importMediaDebug} from './importMediaDebug.js'
+
+const DEFAULT_CONCURRENCY = 6
+
+interface MediaLibraryUploadResult {
+  asset: SanityDocument & {
+    _type: 'sanity.asset'
+    aspects: unknown
+    assetType: FileAsset['_type'] | ImageAsset['_type']
+  }
+  assetInstance: FileAsset | ImageAsset
+}
+
+interface MediaLibraryUploadResponse {
+  body: MediaLibraryUploadResult
+  type: 'response'
+}
+
+export interface State {
+  /**
+   * The last asset processed.
+   */
+  asset: AssetWithAspects
+  /**
+   * The count of input files.
+   */
+  fileCount: number
+}
+
+interface ResolvedAsset {
+  /**
+   * The ids of the `sanity.asset` documents that currently refer to the asset.
+   *
+   * These documents contain aspects, and reference an asset instance document.
+   */
+  assetIds: string[]
+  isExistingAsset: boolean
+  /**
+   * The original filename of the asset as it appears in the import source.
+   *
+   * Note: Currently includes `images/` or `files/` prefix.
+   */
+  originalFilename: string
+  sha1Hash: string
+}
+
+export type AssetWithAspects<Asset extends ResolvedAsset = ResolvedAsset> = Asset & {
+  aspects: unknown | undefined
+}
+
+interface Options {
+  client: SanityClient
+  replaceAspects: boolean
+  sourcePath: string
+  spinner: ReturnType<typeof spinner>
+}
+
+interface Context extends Options {
+  ndjson: () => ReadStream
+  workingPath: string
+}
+
+export function importer(options: Options): Observable<State> {
+  return resolveSource(options).pipe(
+    mergeMap(({aspectsNdjsonPath, files, images, workingPath}) => {
+      const fileCount = files.length + images.length
+
+      if (fileCount === 0) {
+        throw new Error('No assets to import')
+      }
+
+      const context: Context = {
+        ...options,
+        ndjson: () => createReadStream(aspectsNdjsonPath),
+        workingPath,
+      }
+
+      return from(files).pipe(
+        switchMap((file) => zip(of<'file'>('file'), of(file))),
+        mergeWith(from(images).pipe(switchMap((file) => zip(of<'image'>('image'), of(file))))),
+        fetchExistingAssets(context),
+        uploadAsset(context),
+        resolveAspectData(context),
+        setAspects(context),
+        map((asset) => ({
+          asset,
+          fileCount,
+        })),
+      )
+    }),
+  )
+}
+
+/**
+ * @internal
+ */
+export function resolveSource({sourcePath}: Pick<Context, 'sourcePath'>): Observable<{
+  aspectsNdjsonPath: string
+  files: string[]
+  images: string[]
+  workingPath: string
+}> {
+  return from(fs.stat(sourcePath)).pipe(
+    switchMap((stats) => {
+      return stats.isDirectory()
+        ? of(sourcePath)
+        : from(mkdtemp(path.join(tmpdir(), 'sanity-media-library-import'))).pipe(
+            switchMap((tempPath) => {
+              return from(
+                pipeline(createReadStream(sourcePath), gunzipMaybe(), untarMaybe(tempPath)),
+              ).pipe(map(() => tempPath))
+            }),
+          )
+    }),
+    switchMap((importSourcePath) => {
+      return from(
+        glob(['**/data.ndjson'], {
+          absolute: true,
+          cwd: importSourcePath,
+          deep: 2,
+        }),
+      ).pipe(
+        map(([aspectsNdjsonPath]) => ({
+          aspectsNdjsonPath,
+          importSourcePath,
+          workingPath:
+            aspectsNdjsonPath === undefined ? importSourcePath : path.dirname(aspectsNdjsonPath),
+        })),
+      )
+    }),
+    tap(({aspectsNdjsonPath, importSourcePath}) => {
+      if (aspectsNdjsonPath === undefined) {
+        throw new Error(
+          `No ${chalk.bold('data.ndjson')} file found in import source ${chalk.bold(importSourcePath)}`,
+        )
+      }
+      importMediaDebug(`[Found NDJSON file] ${aspectsNdjsonPath}`)
+    }),
+    switchMap(({aspectsNdjsonPath, workingPath}) => {
+      return from(
+        Promise.all([
+          glob(['files/*'], {
+            cwd: workingPath,
+          }),
+          glob(['images/*'], {
+            cwd: workingPath,
+          }),
+        ]),
+      ).pipe(
+        map(([files, images]) => ({
+          aspectsNdjsonPath,
+          files,
+          images,
+          workingPath,
+        })),
+      )
+    }),
+  )
+}
+
+/**
+ * Untar the stream if its contents appear to be tarred.
+ *
+ * @internal
+ */
+function untarMaybe(outputPath: string) {
+  // @ts-expect-error `peek-stream` module currently untyped
+  return peek({maxBuffer: 300, newline: false}, (data, swap) => {
+    if (isTar(data)) {
+      return swap(null, tar.extract(outputPath))
+    }
+
+    return swap(null)
+  })
+}
+
+/**
+ * Fetch the ids of all asset documents that reference the input asset.
+ * The input asset is identified by its SHA-1 hash.
+ *
+ * @internal
+ */
+function fetchAssetsByHash({
+  client,
+  type,
+}: {
+  client: SanityClient
+  type: 'file' | 'image'
+}): OperatorFunction<string, [hash: string, assetIds: string[]]> {
+  return switchMap((hash) =>
+    client.observable
+      .fetch<string[]>(
+        `*[
+          _type == "sanity.asset" &&
+          currentVersion._ref == *[
+            _type == $type &&
+            sha1hash == $hash
+          ][0]._id
+        ]._id`,
+        {
+          hash,
+          type: ['sanity', `${type}Asset`].join('.'),
+        },
+        {
+          tag: 'asset.getId',
+        },
+      )
+      .pipe(switchMap((assetIds) => zip(of(hash), of(assetIds)))),
+  )
+}
+
+function fetchExistingAssets({
+  client,
+  workingPath,
+}: Context): OperatorFunction<
+  [type: 'file' | 'image', asset: string],
+  [type: 'file' | 'image', asset: string, hash: string] | ResolvedAsset
+> {
+  return mergeMap(([type, asset]) => {
+    const createSha1Hash = createHash('sha1')
+
+    const sha1hash = text(
+      createReadStream(path.join(workingPath, asset)).pipe(createSha1Hash).setEncoding('hex'),
+    )
+
+    return from(sha1hash).pipe(
+      tap((hash) =>
+        importMediaDebug(`[Asset ${asset}] Checking for ${type} asset with hash ${hash}`),
+      ),
+      fetchAssetsByHash({client, type}),
+      map<
+        [string, string[]],
+        [type: 'file' | 'image', asset: string, hash: string] | ResolvedAsset
+      >(([hash, assetIds]) => {
+        if (assetIds.length === 0) {
+          return [type, asset, hash]
+        }
+
+        return {
+          assetIds,
+          isExistingAsset: true,
+          originalFilename: asset,
+          sha1Hash: hash,
+        }
+      }),
+    )
+  })
+}
+
+/**
+ * Find the first matching entry in the provided NDJSON stream and attach it to the asset object.
+ *
+ * @internal
+ */
+function resolveAspectData({ndjson}: Context): OperatorFunction<ResolvedAsset, AssetWithAspects> {
+  return mergeMap((resolvedAsset) =>
+    from(
+      findNdjsonEntry<{aspects: unknown}>(
+        ndjson(),
+        (line) =>
+          typeof line === 'object' &&
+          line !== null &&
+          'filename' in line &&
+          line.filename === resolvedAsset.originalFilename,
+      ),
+    ).pipe(
+      map((aspectsFromImport) => ({
+        ...resolvedAsset,
+        aspects: aspectsFromImport?.aspects,
+      })),
+    ),
+  )
+}
+
+// TODO: Batch mutations to reduce HTTP request count.
+export function setAspects({
+  client,
+  replaceAspects,
+}: Pick<Context, 'client' | 'replaceAspects'>): OperatorFunction<
+  AssetWithAspects,
+  AssetWithAspects
+> {
+  return mergeMap((asset) => {
+    const {aspects, assetIds, isExistingAsset} = asset
+
+    if (isExistingAsset && !replaceAspects) {
+      importMediaDebug(`[Asset ${asset.originalFilename}] Skipping replacement of existing aspects`)
+      return of(asset)
+    }
+
+    if (aspects === undefined) {
+      importMediaDebug(`[Asset ${asset.originalFilename}] No aspects to import`)
+      return of(asset)
+    }
+
+    const transaction = client.observable.transaction()
+    for (const assetId of assetIds) {
+      transaction.patch(assetId, {set: {aspects}})
+    }
+
+    importMediaDebug(
+      `[Asset ${asset.originalFilename}] Setting aspects on asset documents ${JSON.stringify(assetIds)}`,
+    )
+
+    return transaction
+      .commit({
+        tag: 'asset.setAspects',
+        visibility: 'async',
+      })
+      .pipe(map(() => asset))
+  }, DEFAULT_CONCURRENCY)
+}
+
+function uploadAsset({
+  client,
+  workingPath,
+}: Context): OperatorFunction<
+  [type: 'file' | 'image', asset: string, hash: string] | ResolvedAsset,
+  ResolvedAsset
+> {
+  return mergeMap((maybeResolvedAsset) => {
+    if ('assetIds' in maybeResolvedAsset) {
+      importMediaDebug(
+        `[Asset ${maybeResolvedAsset.originalFilename}] Skipping upload of existing asset with hash ${maybeResolvedAsset.sha1Hash}`,
+      )
+      return of(maybeResolvedAsset)
+    }
+
+    const [type, asset, hash] = maybeResolvedAsset
+    importMediaDebug(`[Asset ${asset}] Uploading new asset`)
+
+    return client.observable.assets
+      .upload(type, createReadStream(path.join(workingPath, asset)), {
+        tag: 'asset.upload',
+      })
+      .pipe(
+        catchError((error) => {
+          // An asset matching the hash was not found during previous steps, but appears to exist upon upload.
+          //
+          // This may occur if:
+          //   - The asset was uploaded by another client since the check was performed.
+          //   - The asset instance document exists, but is not referenced by any asset document.
+          if (error.statusCode === 409) {
+            importMediaDebug(
+              `[Asset ${asset}] Cannot overwrite existing ${type} asset with hash ${hash}`,
+            )
+            return EMPTY
+          }
+          return EMPTY
+        }),
+        filter((response) => response.type === 'response'),
+        tap(() => importMediaDebug(`[Asset ${asset}] Finished uploading new asset`)),
+        // TODO: The `client.assets.upload` method should return `MediaLibraryUploadResponse` when operating on Media Library resources. When that occurs, this type assertion can be removed.
+        map((response) => (response as unknown as MediaLibraryUploadResponse).body),
+        map<MediaLibraryUploadResult, ResolvedAsset>((result) => ({
+          assetIds: [result.asset._id],
+          isExistingAsset: false,
+          originalFilename: asset,
+          sha1Hash: hash,
+        })),
+      )
+  }, DEFAULT_CONCURRENCY)
+}
---
+++ packages/@sanity/cli/src/commands/media/import.ts
@@ -0,0 +1,189 @@
+// import {input} from '@inquirer/prompts'
+import {Args, Flags} from '@oclif/core'
+import {SanityCommand, spinner} from '@sanity/cli-core'
+import {SanityClient} from '@sanity/client'
+import boxen from 'boxen'
+import chalk from 'chalk'
+import {type OperatorFunction, pipe, scan, tap} from 'rxjs'
+
+import {importer, type State} from '../../actions/media/importMedia.js'
+import {importMediaDebug} from '../../actions/media/importMediaDebug.js'
+import {promptForMediaLibrary} from '../../prompts/promptForMediaLibrary.js'
+import {getMediaLibraries} from '../../services/mediaLibraries.js'
+import {NO_PROJECT_ID} from '../../util/errorMessages.js'
+
+export class MediaImportCommand extends SanityCommand<typeof MediaImportCommand> {
+  static override args = {
+    source: Args.string({
+      description: 'Image file or folder to import from',
+      required: true,
+    }),
+  }
+
+  static override description = 'Import a set of assets to the target media library.'
+
+  static override examples = [
+    {
+      command: '<%= config.bin %> <%= command.id %> products',
+      description: 'Import all assets from the "products" directory',
+    },
+    {
+      command: '<%= config.bin %> <%= command.id %> gallery.tar.gz',
+      description: 'Import all assets from "gallery" archive',
+    },
+    {
+      command: '<%= config.bin %> <%= command.id %> products --replace-aspects',
+      description: 'Import all assets from the "products" directory and replace aspects',
+    },
+  ]
+
+  static override flags = {
+    'media-library-id': Flags.string({
+      description: 'The id of the target media library',
+    }),
+    'replace-aspects': Flags.boolean({
+      description:
+        'Replace existing aspect data. All versions will be replaced (e.g. published and draft aspect data)',
+    }),
+  }
+
+  public async run(): Promise<void> {
+    const {args, flags} = await this.parse(MediaImportCommand)
+    const {source} = args
+    const replaceAspects = flags['replace-aspects']
+
+    const projectId = await this.getProjectId()
+    const cliConfig = await this.getCliConfig()
+    const dataset = cliConfig.api?.dataset
+
+    if (!projectId) {
+      this.error(NO_PROJECT_ID, {
+        exit: 1,
+      })
+    }
+
+    let mediaLibraries
+    try {
+      mediaLibraries = await getMediaLibraries(projectId)
+    } catch (error) {
+      importMediaDebug('Error listing media libraries', error)
+      this.error(
+        `Failed to list media libraries:\n${error instanceof Error ? error.message : error}`,
+        {
+          exit: 1,
+        },
+      )
+    }
+
+    if (mediaLibraries.length === 0) {
+      this.error('No active media libraries found in this project', {exit: 1})
+    }
+
+    let mediaLibraryId = flags['media-library-id']
+    if (!mediaLibraryId) {
+      try {
+        mediaLibraryId = await promptForMediaLibrary({mediaLibraries})
+      } catch (error) {
+        importMediaDebug('Error selecting media library', error)
+        this.error(
+          `Failed to select media library:\n${error instanceof Error ? error.message : error}`,
+          {
+            exit: 1,
+          },
+        )
+      }
+    }
+
+    if (!mediaLibraries.some((library) => library.id === mediaLibraryId)) {
+      this.error(`Media library with id "${mediaLibraryId}" not found`, {exit: 1})
+    }
+
+    const projectClient = await this.getProjectApiClient({
+      apiVersion: 'v2025-02-19',
+      dataset,
+      perspective: 'drafts',
+      projectId,
+      requestTagPrefix: 'sanity.mediaLibraryCli.import',
+      requireUser: true,
+      '~experimental_resource': {
+        id: mediaLibraryId,
+        type: 'media-library',
+      },
+    })
+
+    this.log(
+      boxen(
+        `
+          Importing to media library: ${mediaLibraryId.padEnd(37)}
+          Importing from path: ${source}
+        `,
+        {
+          borderColor: 'yellow',
+          borderStyle: 'round',
+        },
+      ),
+    )
+
+    const spin = spinner('Beginning import…').start()
+
+    await this.importAssets({projectClient, replaceAspects, source, spin})
+  }
+
+  private async importAssets(options: {
+    projectClient: SanityClient
+    replaceAspects: boolean
+    source: string
+    spin: ReturnType<typeof spinner>
+  }): Promise<void> {
+    const {projectClient, replaceAspects, source, spin} = options
+
+    return new Promise<void>((resolve, reject) => {
+      const subscription = importer({
+        client: projectClient,
+        replaceAspects,
+        sourcePath: source,
+        spinner: spin,
+      })
+        .pipe(this.reportResult(spin))
+        .subscribe({
+          complete: () => {
+            resolve()
+          },
+          error: (error: unknown) => {
+            const message = error instanceof Error ? error.message : String(error)
+            spin.stop()
+            reject(new Error(message))
+          },
+        })
+
+      // Cleanup on Ctrl+C
+      process.once('SIGINT', () => {
+        subscription.unsubscribe()
+        spin.fail('Import interrupted.')
+        process.exit(130)
+      })
+    }).catch((error) => {
+      this.error(chalk.red(error.message), {exit: 1})
+    })
+  }
+
+  private reportResult(
+    spin: ReturnType<typeof spinner>,
+  ): OperatorFunction<State, [number, State | undefined]> {
+    let previousState: State | undefined
+
+    return pipe(
+      scan<State, [number, State | undefined]>(
+        (processedAssetsCount, state) => [processedAssetsCount[0] + 1, state],
+        [0, undefined],
+      ),
+      tap({
+        complete: () => spin.succeed(`Imported ${previousState?.fileCount} assets`),
+        next: ([processedAssetsCount, state]) => {
+          previousState = state
+          spin.text = `${processedAssetsCount} of ${state?.fileCount} assets imported ${chalk.dim(state?.asset.originalFilename)}`
+        },
+      }),
+    )
+  }
+}
