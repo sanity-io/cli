7e5d1c1a447effb757557c79069a9475900c3973
Cole Peters <cole@colepeters.com>
2025-12-09 13:56:47 -0600

    Update packages/@sanity/cli-core/src/config/cli/types/userViteConfig.ts
    Co-authored-by: Espen Hovlandsdal <espen@hovlandsdal.com>

--- packages/@sanity/cli-core/src/config/cli/types/userViteConfig.ts
+++ packages/@sanity/cli-core/src/config/cli/types/userViteConfig.ts
@@ -1,5 +1,8 @@
 import {type ConfigEnv, type InlineConfig} from 'vite'
 
+/**
+ * @public
+ */
 export type UserViteConfig =
   | ((config: InlineConfig, env: ConfigEnv) => InlineConfig | Promise<InlineConfig>)
   | InlineConfig
