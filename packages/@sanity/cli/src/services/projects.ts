af0fe86b8498e7cb12629371a73cc2727b6c56ef
Matthew Dent <mttdnt@gmail.com>
2025-12-17 23:33:59 -0500

    feat(init): migrating new project creation functionality of init with some reusable actions and services

--- packages/@sanity/cli/src/services/projects.ts
+++ packages/@sanity/cli/src/services/projects.ts
@@ -1,6 +1,56 @@
-import {getProjectCliClient} from '@sanity/cli-core'
+import {debug, getGlobalCliClient, getProjectCliClient} from '@sanity/cli-core'
 
 export const PROJECTS_API_VERSION = '2025-09-22'
+
+export const CREATE_PROJECT_API_VERSION = 'v2025-05-14'
+
+export interface CreateProjectOptions {
+  displayName: string
+
+  metadata?: {
+    coupon?: string
+    integration?: string
+  }
+  organizationId?: string
+  subscription?: {planId: string}
+}
+
+export interface CreateProjectResult {
+  displayName: string
+  projectId: string
+}
+
+/**
+ * Create a new Sanity project
+ */
+export async function createProject(options: CreateProjectOptions): Promise<CreateProjectResult> {
+  const client = await getGlobalCliClient({
+    apiVersion: CREATE_PROJECT_API_VERSION,
+    requireUser: true,
+  })
+
+  try {
+    const response = await client.request({
+      body: {
+        ...options,
+        metadata: {
+          ...options?.metadata,
+          integration: 'cli',
+        },
+      },
+      method: 'POST',
+      uri: '/projects',
+    })
+
+    return {
+      displayName: options.displayName || '',
+      projectId: response.projectId || response.id,
+    }
+  } catch (err) {
+    debug('Error creating project', err)
+    throw err
+  }
+}
 
 export async function getProjectById(projectId: string) {
   const client = await getProjectCliClient({
