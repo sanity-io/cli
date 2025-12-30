0a826687c64ff0d0603d1766f1ad20d24d4a8682
Matthew Dent <mttdnt@gmail.com>
2025-12-30 14:31:10 -0500

    feature(init): migrated code over for grabbing exisiting project details

--- packages/@sanity/cli/src/prompts/promptForDatasetName.ts
+++ packages/@sanity/cli/src/prompts/promptForDatasetName.ts
@@ -4,11 +4,16 @@ import {validateDatasetName} from '../actions/dataset/validateDatasetName.js'
 
 export function promptForDatasetName(
   options: {default?: string; message?: string} = {},
+  existingDatasets: string[] = [],
 ): Promise<string> {
   return input({
     default: options.default,
     message: options.message || 'Dataset name:',
     validate: (name) => {
+      if (existingDatasets.includes(name)) {
+        return 'Dataset name already exists'
+      }
+
       const err = validateDatasetName(name)
       if (err) {
         return err
