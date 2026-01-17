c145526c6f433a1a8882f2009653ef4c56cc0a9a
Binoy Patel <me@binoy.io>
2026-01-17 06:15:15 +0530

    fix(create-sanity): fixes cli bin path (#302)

--- packages/create-sanity/index.js
+++ packages/create-sanity/index.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 import {spawn} from 'node:child_process'
+import {readFile} from 'node:fs/promises'
 import {join} from 'node:path'
+import {fileURLToPath} from 'node:url'
 
 import {moduleResolve} from 'import-meta-resolve'
 
@@ -8,9 +10,18 @@ const args = process.argv.slice(2)
 
 let cliBin
 try {
-  const cliPkgDir = await moduleResolve('@sanity/cli/package.json', import.meta.url)
-  const cliDir = join(cliPkgDir.pathname, '..')
-  cliBin = join(cliDir, 'bin', 'run.js')
+  const cliPkgDir = fileURLToPath(await moduleResolve('@sanity/cli/package.json', import.meta.url))
+  const cliDir = join(cliPkgDir, '..')
+
+  // Read the package.json file and extract the bin path
+  const pJson = await readFile(cliPkgDir, 'utf8')
+  const pkgJson = JSON.parse(pJson)
+  const binPath = pkgJson.bin?.['sanity']
+  if (!binPath) {
+    throw new Error('Failed to resolve `@sanity/cli` package')
+  }
+
+  cliBin = join(cliDir, binPath)
 } catch (err) {
   throw new Error('Failed to resolve `@sanity/cli` package', {cause: err})
 }
