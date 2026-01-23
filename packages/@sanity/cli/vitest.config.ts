42af9c9e763c533915424b952daa1e30b2436de1
Binoy Patel <me@binoy.io>
2026-01-22 22:35:26 -0500

    test: update coverage exclude paths

--- packages/@sanity/cli/vitest.config.ts
+++ packages/@sanity/cli/vitest.config.ts
@@ -4,16 +4,6 @@ import {defineConfig} from 'vitest/config'
 export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
-    coverage: {
-      exclude: [
-        '**/dist/**',
-        '**/tmp/**',
-        '**/test/**',
-        '**/__tests__/**',
-        '**/*.{test,spec}.{js,ts}',
-      ],
-      provider: 'istanbul',
-    },
     disableConsoleIntercept: true, // helps @oclif/test helpers
     env: {
       OCLIF_TEST_ROOT: 'packages/@sanity/cli',
