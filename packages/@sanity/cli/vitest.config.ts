074fd072c922928583ac3243360d8c81f1136204
Binoy Patel <me@binoy.io>
2026-01-22 22:49:45 -0500

    chore: fix depcheck

--- packages/@sanity/cli/vitest.config.ts
+++ packages/@sanity/cli/vitest.config.ts
@@ -4,6 +4,9 @@ import {defineConfig} from 'vitest/config'
 export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
+    coverage: {
+      provider: 'istanbul',
+    },
     disableConsoleIntercept: true, // helps @oclif/test helpers
     env: {
       OCLIF_TEST_ROOT: 'packages/@sanity/cli',
