a08053854164508e82014522baf1e25d553ff61a
Espen Hovlandsdal <espen@hovlandsdal.com>
2026-02-02 16:06:07 -0800

    fix(cli-core): check `default` export from config import

--- packages/@sanity/cli-core/src/config/studio/getStudioWorkspaces.ts
+++ packages/@sanity/cli-core/src/config/studio/getStudioWorkspaces.ts
@@ -1,9 +1,12 @@
+import {dirname} from 'node:path'
+
 import {firstValueFrom, of} from 'rxjs'
 import {type Workspace} from 'sanity'
 
 import {doImport} from '../../util/doImport.js'
 import {getEmptyAuth} from '../../util/getEmptyAuth.js'
 import {resolveLocalPackage} from '../../util/resolveLocalPackage.js'
+import {isStudioConfig} from './isStudioConfig.js'
 
 /**
  * Resolves the workspaces from the studio config.
@@ -13,9 +16,17 @@ import {resolveLocalPackage} from '../../util/resolveLocalPackage.js'
  * @internal
  */
 export async function getStudioWorkspaces(configPath: string): Promise<Workspace[]> {
-  const config = await doImport(configPath)
+  let config = await doImport(configPath)
+  if (!isStudioConfig(config)) {
+    if (!('default' in config) || !isStudioConfig(config.default)) {
+      throw new TypeError(`Invalid studio config format in "${configPath}"`)
+    }
 
-  const {resolveConfig} = await resolveLocalPackage<typeof import('sanity')>('sanity', configPath)
+    config = config.default
+  }
+
+  const workDir = dirname(configPath)
+  const {resolveConfig} = await resolveLocalPackage<typeof import('sanity')>('sanity', workDir)
   if (typeof resolveConfig !== 'function') {
     throw new TypeError('Expected `resolveConfig` from `sanity` to be a function')
   }
@@ -26,9 +37,10 @@ export async function getStudioWorkspaces(configPath: string): Promise<Workspace
     ? config
     : [{...config, basePath: config.basePath || '/', name: config.name || 'default'}]
 
-  rawWorkspaces.map((workspace) => {
-    workspace.auth = {state: of(getEmptyAuth())}
-  })
+  const unauthedWorkspaces = rawWorkspaces.map((workspace) => ({
+    ...workspace,
+    auth: {state: of(getEmptyAuth())},
+  }))
 
-  return firstValueFrom(resolveConfig(rawWorkspaces))
+  return firstValueFrom(resolveConfig(unauthedWorkspaces))
 }
