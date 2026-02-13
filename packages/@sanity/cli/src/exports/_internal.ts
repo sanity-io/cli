d3526948ed60ee4d4d7caab80a1897c932aedc44
Espen Hovlandsdal <espen@hovlandsdal.com>
2026-02-13 14:11:40 -0800

    chore: export missing utils/types re-exported from `sanity/cli`

--- packages/@sanity/cli/src/exports/_internal.ts
+++ packages/@sanity/cli/src/exports/_internal.ts
@@ -1,1 +1,3 @@
+export {getStudioEnvironmentVariables} from '../actions/build/getStudioEnvironmentVariables'
+export type {StudioEnvVariablesOptions} from '../actions/build/getStudioEnvironmentVariables'
 export {extractManifestSchemaTypes} from '../actions/manifest/extractWorkspaceManifest'
