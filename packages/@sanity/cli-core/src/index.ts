0613b186d4a68dcbcc061c1516d9233306b4f4a0
Binoy Patel <me@binoy.io>
2026-02-14 11:30:25 -0500

    test: add tests for cli config definers (#419)

--- packages/@sanity/cli-core/src/index.ts
+++ packages/@sanity/cli-core/src/index.ts
@@ -1,6 +1,5 @@
 export * from './config/cli/getCliConfig.js'
 export * from './config/cli/getCliConfigSync.js'
-export {cliConfigSchema} from './config/cli/schemas.js'
 export {type CliConfig} from './config/cli/types/cliConfig.js'
 export {type UserViteConfig} from './config/cli/types/userViteConfig.js'
 export * from './config/findProjectRoot.js'
