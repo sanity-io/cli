e8ba233127b8c836bb938df40b001331100c9588
Binoy Patel <me@binoy.io>
2026-02-19 16:47:33 -0500

    fix(build): fixes issue with build failure on missing favicons

--- packages/@sanity/cli/src/actions/build/writeFavicons.ts
+++ packages/@sanity/cli/src/actions/build/writeFavicons.ts
@@ -1,23 +1,29 @@
 import fs from 'node:fs/promises'
-import path, {dirname} from 'node:path'
-import {fileURLToPath} from 'node:url'
+import path from 'node:path'
 
+import {subdebug} from '@sanity/cli-core'
 import {readPackageUp} from 'read-package-up'
 
 import {copyDir} from '../../util/copyDir.js'
 import {writeWebManifest} from './writeWebManifest.js'
 
-export async function writeFavicons(basePath: string, destDir: string): Promise<void> {
-  const dir = dirname(fileURLToPath(import.meta.url))
+const debug = subdebug('writeFavicons')
 
-  const sanityPkgPath = (await readPackageUp({cwd: dir}))?.path
+export async function writeFavicons(basePath: string, destDir: string): Promise<void> {
+  const sanityPkgPath = (await readPackageUp({cwd: import.meta.dirname}))?.path
+
+  debug('sanityPkgPath: %s', sanityPkgPath)
   const faviconsPath = sanityPkgPath
     ? path.join(path.dirname(sanityPkgPath), 'static', 'favicons')
     : undefined
 
+  debug('faviconsPath: %s', faviconsPath)
+
   if (!faviconsPath) {
     throw new Error('Unable to resolve `sanity` module root')
   }
+
+  debug('destDir: %s', destDir)
 
   await fs.mkdir(destDir, {recursive: true})
   await copyDir(faviconsPath, destDir, true)
