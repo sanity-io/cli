287c0829cb9434239f1fb04f2ce301780653d7b0
Espen Hovlandsdal <espen@hovlandsdal.com>
2026-02-26 16:42:23 -0800

    fix: use `stdin`, not `stdout` for interactive checks (#479)

--- packages/@sanity/cli-core/src/util/isInteractive.ts
+++ packages/@sanity/cli-core/src/util/isInteractive.ts
@@ -1,3 +1,3 @@
 export function isInteractive(): boolean {
-  return process.stdout.isTTY && process.env.TERM !== 'dumb' && !('CI' in process.env)
+  return Boolean(process.stdin.isTTY) && process.env.TERM !== 'dumb' && !('CI' in process.env)
 }
