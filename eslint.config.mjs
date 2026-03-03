d95ecfdb6f7db9eaad042ec38606841eaa5aa922
Espen Hovlandsdal <espen@hovlandsdal.com>
2026-03-03 11:00:43 -0800

    chore: lint ignore prebuilt fixtures

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,4 +3,8 @@ import {resolve} from 'node:path'
 import {includeIgnoreFile} from '@eslint/compat'
 import eslintConfig from '@sanity/eslint-config-cli'
 
-export default [includeIgnoreFile(resolve(import.meta.dirname, '.gitignore')), ...eslintConfig]
+export default [
+  includeIgnoreFile(resolve(import.meta.dirname, '.gitignore')),
+  {ignores: ['**/fixtures/prebuilt-*/dist/**']},
+  ...eslintConfig,
+]
