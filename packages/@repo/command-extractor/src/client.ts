54bcde96ca29b031e4b073ecd8487e296388c32a
Binoy Patel <me@binoy.io>
2026-03-11 09:17:46 -0400

    chore(ci): removes command extraction

--- packages/@repo/command-extractor/src/client.ts
+++
@@ -1,8 +0,0 @@
-import {createClient} from '@sanity/client'
-
-import {SANITY_CONFIG} from './constants.js'
-
-export const client = createClient({
-  ...SANITY_CONFIG,
-  token: process.env.SANITY_API_TOKEN,
-})
