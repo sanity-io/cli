87b733ffb3922d4d492e569918b6087e302f39e9
Espen Hovlandsdal <espen@hovlandsdal.com>
2026-03-12 20:07:14 -0700

    fix: bump react+react-dom to latest on new installs (#658)

--- packages/@sanity/cli/src/actions/init/sdkAppDependencies.ts
+++ packages/@sanity/cli/src/actions/init/sdkAppDependencies.ts
@@ -3,13 +3,13 @@ export const sdkAppDependencies = {
     // change these to 'latest' as in studioDependencies.ts once SDK v3 is released
     '@sanity/sdk': '^2',
     '@sanity/sdk-react': '^2',
-    react: '^19.2',
-    'react-dom': '^19.2',
+    react: '^19.2.4',
+    'react-dom': '^19.2.4',
   },
 
   devDependencies: {
     '@sanity/eslint-config-studio': 'latest',
-    '@types/react': '^19.1',
+    '@types/react': '^19.2.14',
     eslint: '^9.28',
     prettier: '^3.5',
     sanity: 'latest',
