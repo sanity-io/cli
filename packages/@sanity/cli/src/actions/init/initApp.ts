40aaaf7af7a868b18272ceeb929d61c3bf2042fc
Gustav Hansen <gu@stav.dev>
2026-04-21 14:02:13 +0200

    feat(init): add promt for federation (#988)
    * feat(init): add promt for federation
    
    * chore: update auto-generated changeset for PR #988
    
    ---------
    
    Co-authored-by: squiggler-app[bot] <265501495+squiggler-app[bot]@users.noreply.github.com>

--- packages/@sanity/cli/src/actions/init/initApp.ts
+++ packages/@sanity/cli/src/actions/init/initApp.ts
@@ -15,6 +15,7 @@ import {type InitOptions} from './types.js'
 export async function initApp({
   datasetName,
   defaults,
+  federation,
   mcpConfigured,
   options,
   organizationId,
@@ -28,6 +29,7 @@ export async function initApp({
 }: {
   datasetName: string
   defaults: {projectName: string}
+  federation: boolean
   mcpConfigured: EditorName[]
   options: InitOptions
   organizationId: string | undefined
@@ -57,6 +59,7 @@ export async function initApp({
     datasetName,
     defaults,
     displayName: '',
+    federation,
     options,
     organizationId,
     output,
--- packages/@sanity/cli/src/actions/init/initStudio.ts
+++ packages/@sanity/cli/src/actions/init/initStudio.ts
@@ -27,6 +27,7 @@ export async function initStudio({
   datasetName,
   defaults,
   displayName,
+  federation,
   isFirstProject,
   mcpConfigured,
   options,
@@ -42,6 +43,7 @@ export async function initStudio({
   datasetName: string
   defaults: {projectName: string}
   displayName: string
+  federation: boolean
   isFirstProject: boolean
   mcpConfigured: EditorName[]
   options: InitOptions
@@ -91,6 +93,7 @@ export async function initStudio({
     datasetName,
     defaults,
     displayName,
+    federation,
     options,
     organizationId,
     output,
