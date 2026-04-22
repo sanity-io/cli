3386680dac7f441751e209d2a99685939a148190
Gustav Hansen <gu@stav.dev>
2026-04-22 16:01:28 +0200

    feat(init): use `workbench` dist-tag for `sanity` package (#992)
    * feat(init): use `workbench` dist-tag for `sanity` package
    
    * chore: update auto-generated changeset for PR #992
    
    * test: add unit tests
    
    ---------
    
    Co-authored-by: squiggler-app[bot] <265501495+squiggler-app[bot]@users.noreply.github.com>

--- packages/@sanity/cli/src/util/resolveLatestVersions.ts
+++ packages/@sanity/cli/src/util/resolveLatestVersions.ts
@@ -1,5 +1,6 @@
 import {getLatestVersion} from 'get-latest-version'
 import promiseProps from 'promise-props-recursive'
+import semver from 'semver'
 
 /**
  * Resolve the latest versions of given packages within their defined ranges
@@ -12,13 +13,15 @@ export function resolveLatestVersions(
 ): Promise<Record<string, string>> {
   const lookups: Record<string, Promise<string> | string> = {}
   for (const [packageName, range] of Object.entries(pkgs)) {
-    lookups[packageName] =
-      range === 'latest' ? getLatestVersion(packageName, {range}).then(caretify) : range
+    const isDistTag = semver.validRange(range) === null
+    lookups[packageName] = isDistTag
+      ? getLatestVersion(packageName, {range}).then((version) => caretify(version, range))
+      : range
   }
 
   return promiseProps(lookups)
 }
 
-function caretify(version: string | undefined) {
-  return version ? `^${version}` : 'latest'
+function caretify(version: string | undefined, fallback: string) {
+  return version ? `^${version}` : fallback
 }
