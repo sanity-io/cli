3da8ef3420ebdd716828429190fb3d6fb0a0dc6b
Gustav Hansen <gu@stav.dev>
2026-04-22 16:45:49 +0200

    fix(init): do not resolve dist tags (#1000)
    * fix(init): do not resolve dist tags
    
    * chore: update auto-generated changeset for PR #1000
    
    ---------
    
    Co-authored-by: squiggler-app[bot] <265501495+squiggler-app[bot]@users.noreply.github.com>

--- packages/@sanity/cli/src/util/resolveLatestVersions.ts
+++ packages/@sanity/cli/src/util/resolveLatestVersions.ts
@@ -1,6 +1,5 @@
 import {getLatestVersion} from 'get-latest-version'
 import promiseProps from 'promise-props-recursive'
-import semver from 'semver'
 
 /**
  * Resolve the latest versions of given packages within their defined ranges
@@ -13,15 +12,13 @@ export function resolveLatestVersions(
 ): Promise<Record<string, string>> {
   const lookups: Record<string, Promise<string> | string> = {}
   for (const [packageName, range] of Object.entries(pkgs)) {
-    const isDistTag = semver.validRange(range) === null
-    lookups[packageName] = isDistTag
-      ? getLatestVersion(packageName, {range}).then((version) => caretify(version, range))
-      : range
+    lookups[packageName] =
+      range === 'latest' ? getLatestVersion(packageName, {range}).then(caretify) : range
   }
 
   return promiseProps(lookups)
 }
 
-function caretify(version: string | undefined, fallback: string) {
-  return version ? `^${version}` : fallback
+function caretify(version: string | undefined) {
+  return version ? `^${version}` : 'latest'
 }
