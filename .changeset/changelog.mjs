e399041378b71d62a44402b1b321278577d375d6
Binoy Patel <me@binoy.io>
2026-04-28 10:53:45 -0400

    chore: remove extra blank lines between changelog bullet points (#1019)

--- .changeset/changelog.mjs
+++ .changeset/changelog.mjs
@@ -62,9 +62,9 @@ async function getReleaseLine(changeset, _type, options) {
 
   const formatted = `${scopePrefix}${firstLine}${prLink}${commitLink}`
   if (restLines.length > 0) {
-    return `\n- ${formatted}\n${restLines.map((l) => (l ? '  ' + l : l)).join('\n')}`
+    return `- ${formatted}\n${restLines.map((l) => (l ? '  ' + l : l)).join('\n')}`
   }
-  return `\n- ${formatted}`
+  return `- ${formatted}`
 }
 
 async function getDependencyReleaseLine(_changesets, dependenciesUpdated, _options) {
