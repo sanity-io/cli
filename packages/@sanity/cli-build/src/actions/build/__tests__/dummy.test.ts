e9043b3703db9e636a25ed8ce8cdd412a46e8b13
Dave Sewell <snocorp@gmail.com>
2026-05-11 20:50:22 -0400

    Update test with todo

--- packages/@sanity/cli-build/src/actions/build/__tests__/dummy.test.ts
+++ packages/@sanity/cli-build/src/actions/build/__tests__/dummy.test.ts
@@ -1,5 +1,6 @@
 import {describe, test} from 'vitest'
 
+// TODO this test wil be replaced as more files are moved into this package
 describe('placeholder test file', () => {
   test('should pass', () => {})
 })
