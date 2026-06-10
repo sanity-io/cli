6c136e36531bb07f613eb72b36a0ed2eba68ff89
Gustav Hansen <gu@stav.dev>
2026-06-10 11:39:06 +0200

    feat(workbench): thread workbench services through, re-run live in dev (US4) (#1176)

--- packages/@sanity/cli/src/actions/build/buildStaticFiles.ts
+++ packages/@sanity/cli/src/actions/build/buildStaticFiles.ts
@@ -11,7 +11,7 @@ import {
   writeSanityRuntime,
 } from '@sanity/cli-build/_internal/build'
 import {type CliConfig, type UserViteConfig} from '@sanity/cli-core'
-import {type InterfaceArtifact} from '@sanity/federation/vite'
+import {type InterfaceArtifact, type ServiceArtifact} from '@sanity/federation/vite'
 import {type PluginOptions as ReactCompilerConfig} from 'babel-plugin-react-compiler'
 import {build, createBuilder} from 'vite'
 
@@ -46,6 +46,7 @@ interface StaticBuildOptions {
   profile?: boolean
   reactCompiler?: ReactCompilerConfig
   schemaExtraction?: CliConfig['schemaExtraction']
+  services?: readonly ServiceArtifact[]
   sourceMap?: boolean
   views?: readonly InterfaceArtifact[]
   vite?: UserViteConfig
@@ -72,6 +73,7 @@ export async function buildStaticFiles(
     outputDir,
     reactCompiler,
     schemaExtraction,
+    services,
     sourceMap = false,
     views,
     vite: extendViteConfig,
@@ -99,6 +101,7 @@ export async function buildStaticFiles(
       mode,
       outputDir,
       reactCompiler,
+      services,
       sourceMap,
       views,
     })
