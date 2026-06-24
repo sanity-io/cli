bc9b295764c04684f0904ae978ff5a318e220e70
Dave Sewell <snocorp@gmail.com>
2026-06-23 22:54:18 -0400

    Organize exports

--- packages/@sanity/cli/src/exports/_internal.ts
+++ packages/@sanity/cli/src/exports/_internal.ts
@@ -1,5 +1,5 @@
+export {extractManifestSchemaTypes} from '../actions/manifest/extractWorkspaceManifest.js'
 export {
   getStudioEnvironmentVariables,
   type StudioEnvVariablesOptions,
 } from '@sanity/cli-build/_internal/build'
-export {extractManifestSchemaTypes} from '../actions/manifest/extractWorkspaceManifest.js'
