86aecb0c8a96841e8481200d98d7c20172c721e8
filmaj <fil.maj@sanity.io>
2026-06-29 11:21:15 -0400

    chore(build): dont use export * in cli-core

--- packages/@sanity/cli-core/src/_exports/ux.ts
+++ packages/@sanity/cli-core/src/_exports/ux.ts
@@ -1,5 +1,30 @@
 export {NonInteractiveError} from '../errors/NonInteractiveError.js'
-export * from '../ux/boxen.js'
-export * from '../ux/logSymbols.js'
-export * from '../ux/prompts.js'
-export * from '../ux/spinner.js'
+export {
+  boxen,
+  type BoxenOptions,
+  type Boxes,
+  type CustomBorderStyle,
+  type Spacing,
+} from '../ux/boxen.js'
+export {logSymbols} from '../ux/logSymbols.js'
+export {
+  checkbox,
+  confirm,
+  editor,
+  expand,
+  input,
+  number,
+  password,
+  rawlist,
+  search,
+  select,
+  Separator,
+} from '../ux/prompts.js'
+export {
+  spinner,
+  type Spinner,
+  type SpinnerInstance,
+  type SpinnerOptions,
+  spinnerPromise,
+  type SpinnerPromiseOptions,
+} from '../ux/spinner.js'
