768312dfdc8de68433f7cb4837f3b16e7108d85f
filmaj <fil.maj@sanity.io>
2026-06-30 10:44:37 -0400

    test(chore): reorganizing cli-core exports

---
+++ packages/@sanity/cli-core/src/_exports/types.ts
@@ -0,0 +1,6 @@
+export {
+  type CLITelemetryStore,
+  type ConsentInformation,
+  type TelemetryUserProperties,
+} from '../telemetry/types.js'
+export {type Output, type RequireProps, type SanityOrgUser} from '../types.js'
