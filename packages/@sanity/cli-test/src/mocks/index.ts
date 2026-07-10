7353c4f81690ca4cad8710d40c45ca0acceccd44
filmaj <fil.maj@sanity.io>
2026-07-10 07:54:54 -0400

    refactor(chore): drop unused mock files

--- packages/@sanity/cli-test/src/mocks/index.ts
+++
@@ -1,6 +0,0 @@
-export * as apiClientMocks from './cli-core/apiClient.js'
-export * as cliUserConfigMocks from './cli-core/cliUserConfig.js'
-export * as configMocks from './cli-core/config.js'
-export * as packageManagerMocks from './cli-core/package-manager.js'
-export {createMockOutput, MockedSanityCommand, mocks} from './cli-core/SanityCommand.js'
-export * as uxMocks from './cli-core/ux.js'
