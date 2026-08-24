610281c9c17681e70b0b0c4549fa912275949adf
Dave Sewell <snocorp@gmail.com>
2026-08-24 18:19:19 -0400

    test: add mocks for cli-build manfest exports

---
+++ packages/@sanity/cli-test/src/mocks/cli-build/manifest.ts
@@ -0,0 +1,9 @@
+import {type Mock, vi} from 'vitest'
+
+export const extractCoreAppManifest: Mock = vi.fn()
+export const readIconFromPath: Mock = vi.fn()
+export const resolveTitleUpdate: Mock = vi.fn()
+export const extractManifest: Mock = vi.fn()
+export const extractManifestSchemaTypes: Mock = vi.fn()
+export const extractWorkspaceManifest: Mock = vi.fn()
+export const writeManifestFile: Mock = vi.fn()
--- packages/@sanity/cli/src/actions/manifest/extractManifest.ts
+++ packages/@sanity/cli/src/actions/manifest/extractManifest.ts
@@ -1,11 +1,11 @@
-import {extractManifest as interalExtractManifest} from '@sanity/cli-build/_internal/manifest'
+import {extractManifest as internalExtractManifest} from '@sanity/cli-build/_internal/manifest'
 
-type ExtractManifestOptions = Parameters<typeof interalExtractManifest>[0]
+type ExtractManifestOptions = Parameters<typeof internalExtractManifest>[0]
 
 export async function extractManifest({
   outPath,
   path,
   workDir,
 }: ExtractManifestOptions): Promise<void> {
-  await interalExtractManifest({outPath, path, workDir})
+  await internalExtractManifest({outPath, path, workDir})
 }
