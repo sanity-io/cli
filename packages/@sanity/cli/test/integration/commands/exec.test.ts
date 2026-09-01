43f961225d4a21e6d477e794f65bdaa03b27bffd
Jonah Snider <jonah@jonahsnider.com>
2026-09-01 14:06:13 -0700

    test: add test for TypeScript path aliases
    Signed-off-by: Jonah Snider <jonah@jonahsnider.com>

--- packages/@sanity/cli/test/integration/commands/exec.test.ts
+++ packages/@sanity/cli/test/integration/commands/exec.test.ts
@@ -1,5 +1,5 @@
 import {type SpawnOptions} from 'node:child_process'
-import {copyFile, mkdir, rm} from 'node:fs/promises'
+import {copyFile, mkdir, rm, writeFile} from 'node:fs/promises'
 import {tmpdir} from 'node:os'
 import {join, resolve} from 'node:path'
 
@@ -121,6 +121,34 @@ describe('#exec', {timeout: 15 * 1000}, () => {
       expect(data.success).toBe(true)
       expect(data.env.SANITY_BASE_PATH).toBe(exampleDir)
       expect(data.argv).toEqual([process.argv[0], scriptPath, '--dry-run', 'positional-argument'])
+    })
+
+    test.skipIf(isWindowsNode24OrUp)('resolves TypeScript path aliases', async (t) => {
+      await mkdir(join(exampleDir, 'test-alias'))
+      await writeFile(
+        join(exampleDir, 'test-alias', 'value.ts'),
+        "export const value = 'resolved'\n",
+      )
+      await writeFile(
+        join(exampleDir, 'tsconfig.json'),
+        JSON.stringify({compilerOptions: {baseUrl: '.', paths: {'@test/*': ['./test-alias/*']}}}),
+      )
+      await writeFile(
+        scriptPath,
+        "import {value} from '@test/value'\n\nconsole.log(JSON.stringify({value}))\n",
+      )
+
+      const {error, stderr, stdout} = await testCommand(ExecCommand, [scriptPath])
+      t.onTestFailed(() => {
+        // eslint-disable-next-line no-console
+        console.log(stdout)
+        // eslint-disable-next-line no-console
+        console.warn(stderr)
+      })
+
+      if (error) throw error
+
+      expect(JSON.parse(stdout.trim())).toEqual({value: 'resolved'})
     })
 
     test.skipIf(!TEST_TOKEN)('executes script with --with-user-token flag', async (t) => {
